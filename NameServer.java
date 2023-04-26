ccf68d0fb2f9b59b52e52b2544189f8dd72bf4d5
Soumya130498 <113308735+Soumya130498@users.noreply.github.com>
2023-04-26 11:57:43 -0400

    Update NameServer.java
    added enterRing function

--- NameServer.java
+++ NameServer.java
@@ -9,14 +9,17 @@ import java.util.Scanner;
 
 public class NameServer {
     HashMap<Integer, String> data = new HashMap<>();
-    static ServerSocket serverSocket;
-    static Socket socket;
-    static int ID;
-    static int serverPort;
+    static ServerSocket nsSocket;
+    static Socket bootStrapSocket;
+    static int nsID;
+    static int nsPort;
+    static int bootStrapPort;
+    static String bootStrapIP;
     NSOperations nsOperations;
 
+
     public NameServer() {
-        nsOperations = new NSOperations(data, ID, serverPort);
+        nsOperations = new NSOperations(data, nsID, nsPort);
     }
 
 
@@ -24,16 +27,41 @@ public class NameServer {
         File confFile = new File(args[0]);
 //        read config from file
         Scanner scanner = new Scanner(confFile);
-        ID = Integer.parseInt(scanner.nextLine());
-        serverPort = Integer.parseInt(scanner.nextLine());
-        serverSocket = new ServerSocket(serverPort);
+        nsID = Integer.parseInt(scanner.nextLine());
+        nsPort = Integer.parseInt(scanner.nextLine());
+        String bs = scanner.nextLine();
+        bootStrapIP = bs.split(" ")[0];
+        bootStrapPort = Integer.parseInt(bs.split(" ")[1]);
+
         NameServer nameServer = new NameServer();
         new Thread(new NameServerUI(nameServer)).start();
     }
+    
+    public void enterRing() throws UnknownHostException, IOException {
+        bootStrapSocket = new Socket(bootStrapIP, bootStrapPort);
+        DataOutputStream dos = new DataOutputStream(bootStrapSocket.getOutputStream());
+        DataInputStream dis = new DataInputStream(bootStrapSocket.getInputStream());
+        //String nsIP = Inet4Address.getLocalHost().getHostAddress();
+        String nsIP = nsOperations.nsMeta.getIP();
 
-    public void enterRing() {
+        dos.writeUTF("enter " + nsID + nsIP + nsPort);
+
+        String predInfo = dis.readUTF();
+        String succInfo = dis.readUTF();
+
+
+        int predecessorID = Integer.parseInt(predInfo.split(" ")[1]);
+        String predecessorIP = predInfo.split(" ")[1];
+        int predecessorPort = Integer.parseInt(predInfo.split(" ")[1]);
+        int successorID = Integer.parseInt(succInfo.split(" ")[1]);
+        String successorIP = succInfo.split(" ")[1];
+        int successorPort = Integer.parseInt(succInfo.split(" ")[1]);
+
+        // NSMeta nsMeta = new NSMeta(nsID, nsPort);
+        nsOperations.nsMeta.setRingMeta(predecessorID, predecessorIP, predecessorPort, successorID, successorIP, successorPort);
+
     }
-
+    
     public void exitRing() {
     }
 }
